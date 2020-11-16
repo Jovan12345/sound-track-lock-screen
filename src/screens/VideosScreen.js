5eebdc0082fdc0223164a52b3675b83343899005
Jovan12345 <jovanj@gecko.mk>
2020-11-16 16:56:53 +0100

    changes on Videos Screen

--- src/screens/VideosScreen.js
+++ src/screens/VideosScreen.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import youtube from '../api/youtube';
 
 const KEY = 'AIzaSyDrS6aoqJK0t4-JNQsBwwG1keWLJZUnRm4'
 
@@ -18,12 +19,31 @@ const onTermSubmit = async (term) => {
 }
 
 export default function Videos() {
+    const [term, setTerm] = useState('')
     return (
-        <View>
-            <Text>Search videos</Text>
-            <Button title="Search" onPress={() => onTermSubmit('buildings')} />
+        <View style={{ flex: 1, backgroundColor: 'lightblue' }}>
+            <View style={styles.container}>
+                <Text style={styles.textStyle}>Search videos</Text>
+                <TextInput value={term} placeholder="Search Video" onChangeText={setTerm} />
+                <Button title="Search" onPress={() => {
+                    onTermSubmit(term)
+                    setTerm('')
+                }} />
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 30,
+        justifyContent: "center",
+        borderWidth: 2,
+        borderColor: 'grey'
+    },
+    textStyle: {
+        fontSize: 26,
+        alignSelf: 'center'
+    }
+})
